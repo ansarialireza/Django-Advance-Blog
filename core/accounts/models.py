0d1ef5a9591f301afd3d057ac2a1a9e2194a228b
alireza <58270983+ansarialireza@users.noreply.github.com>
2024-07-29 11:25:29 +0330

    ✅ add User models

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin,BaseUserManager
 # Create your models here.
+
+class User(AbstractBaseUser,PermissionsMixin):
+    email = models.EmailField(max_length=255,unique=True)
+    is_staff = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    
+    USERNAME_FIELD='email'
+    
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
+    
+    def __str__ (self):
+        return self.email
